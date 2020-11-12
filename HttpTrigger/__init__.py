c6882a6fd7ebbf7afffb9abe8f994e480e3e37db
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-11-12 14:31:27 -0800

    Initial Commit

---
+++ HttpTrigger/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+import azure.functions as func
+from flask import Flask
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    app = Flask(__name__)
+    logging.info(f'Flask is loaded with app name {app.name}')
+
+    return func.HttpResponse(
+        f"Hello World!",
+        status_code=200
+    )
